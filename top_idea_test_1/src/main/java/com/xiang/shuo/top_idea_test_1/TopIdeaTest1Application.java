7f71f3ebd2a668b2f15d4b147f590b4fac6cb971
xiangshuo <hongpiwa@163.com>
2018-03-05 14:47:01 +0800

    XS-1 文件同步测试

---
+++ top_idea_test_1/src/main/java/com/xiang/shuo/top_idea_test_1/TopIdeaTest1Application.java
@@ -0,0 +1,12 @@
+package com.xiang.shuo.top_idea_test_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopIdeaTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopIdeaTest1Application.class, args);
+	}
+}
